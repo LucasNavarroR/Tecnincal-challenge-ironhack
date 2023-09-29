e6eec13f0bb29a374c6158ba4603891acc85e516
Lucas Navarro <navarrorlucas@gmail.com>
2023-09-29 11:58:34 +0200

    backend falta img

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
+const phoneList = require("../data/phones.json")
+console.log(phoneList)
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get("/phones", (req, res, next) => {
+  res.json(phoneList);
+});
+
+router.get("/phones/:id", (req, res, next) => {
+
+ const phoneId = req.params.id
+console.log(phoneId)
+ const phoneDetails =  phoneList.find( phone => phone.id === phoneId)
+ console.log(phoneDetails)
+
+
+
+  res.json(phoneDetails);
 });
 
 module.exports = router;
